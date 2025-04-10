0f88d12604558c70a201e7a3d3545bbf4f8671eb
Arthur Chardon <arthur.chardon74@gmail.com>
2025-04-10 21:48:48 +0200

    style: login page updated, add fonts and favicon

--- frontend/src/components/Login/Login.tsx
+++ frontend/src/components/Login/Login.tsx
@@ -1,49 +1,65 @@
+import "./Login.css";
+
 const Login = () => {
   return (
-    <div className="flex items-center">
-      <div>
-        <form method="post" action="/api/login">
-          <p>
-            <label htmlFor="username">Username:</label>
-            <input type="text" name="username" id="login-username"></input>
-          </p>
-          <p>
-            <label htmlFor="password">Password:</label>
-            <input type="password" name="password" id="login-password"></input>
-          </p>
-          <p>
-            <button type="submit">Login</button>
-          </p>
-        </form>
-
-        <div className="sso-container">
-          <div className="container">
-            <p>Click the button below to sign in with Google:</p>
-            <a href="/auth/google" className="btn">
-              Sign in with Google
-            </a>
+    <>
+      <div className="flex items-center w-full justify-center gap-[3rem] grow">
+        <div>
+          <div className="container-box bg-white px-[2rem] py-[1.5rem] rounded-xl">
+            <h1 className="text-jet-black">
+              <strong>Log in</strong> to save your progress
+            </h1>
+            <form method="post" action="/api/login">
+              <div className="flex flex-col">
+                <label htmlFor="login-username">Username:</label>
+                <input type="text" name="username" id="login-username"></input>
+              </div>
+              <div className="flex flex-col">
+                <label htmlFor="login-password">Password:</label>
+                <input
+                  type="password"
+                  name="password"
+                  id="login-password"
+                ></input>
+              </div>
+              <div className="flex gap-[2rem] items-center">
+                <button type="submit">Login</button>
+                <a href="/auth/google" className="btn">
+                  Sign in with Google
+                </a>
+              </div>
+            </form>
           </div>
         </div>
-      </div>
 
-      <form method="post" action="/api/signup">
-        <p>
-          <label htmlFor="email">Email:</label>
-          <input type="text" name="email" id="signup-email"></input>
-        </p>
-        <p>
-          <label htmlFor="username">Username:</label>
-          <input type="text" name="username" id="signup-username"></input>
-        </p>
-        <p>
-          <label htmlFor="password">Password:</label>
-          <input type="password" name="password" id="signup-password"></input>
-        </p>
-        <p>
-          <button type="submit">Sign up</button>
-        </p>
-      </form>
-    </div>
+        <div className="container-box bg-white px-[2rem] py-[1.5rem] rounded-xl">
+          <h1>
+            <strong>Sign up</strong> to start your journey
+          </h1>
+          <form method="post" action="/api/signup">
+            <div className="flex flex-col">
+              <label htmlFor="signup-email">Email:</label>
+              <input type="text" name="email" id="signup-email"></input>
+            </div>
+            <div className="flex flex-col">
+              <label htmlFor="signup-username">Username:</label>
+              <input type="text" name="username" id="signup-username"></input>
+            </div>
+            <div className="flex flex-col">
+              <label htmlFor="signup-password">Password:</label>
+              <input
+                type="password"
+                name="password"
+                id="signup-password"
+              ></input>
+            </div>
+            <p>
+              <button type="submit">Sign up</button>
+            </p>
+          </form>
+        </div>
+      </div>
+    </>
   );
 };
 
--- frontend/src/components/Navbar/Navbar.tsx
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -1,30 +1,46 @@
 import { Link } from "react-router-dom";
 import "./Navbar.css";
 import { useAuth } from "../../context/AuthContext";
+import { useEffect, useState } from "react";
 
 const Navbar = () => {
   const user = useAuth()?.user;
+
+  //TODO put it on help page
+  const [greeting, setGreeting] = useState("Welcome!");
+
+  useEffect(() => {
+    const hour = new Date().getHours();
+    if (hour < 7 || hour >= 22) return setGreeting("Good night!");
+    if (hour >= 7 && hour < 13) return setGreeting("Good morning!");
+    if (hour >= 13 && hour < 18) return setGreeting("Good afternoon!");
+    if (hour >= 18 && hour < 22) return setGreeting("Good evening!");
+  }, []);
+
   return (
-    <nav className="flex gap-[1rem] p-[1rem] justify-end">
-      <Link to="/">
-        <img src="letter-w.svg" width="50" height="50"></img>
-      </Link>
-      {!user && (
-        <Link to="/login">
-          <img src="login.svg" width="50" height="50"></img>
+    <>
+      <nav className="flex gap-[1rem] p-[1rem] justify-end items-center">
+        <h2 className="mr-[5rem] block opacity-0">{greeting}</h2>
+        <Link to="/">
+          <img src="letter-w.svg" width="50" height="50"></img>
         </Link>
-      )}
-      {user && (
-        <>
-          <Link to="/profile">
-            <img src="person.svg" width="50" height="50"></img>
+        {!user && (
+          <Link to="/login">
+            <img src="login.svg" width="50" height="50"></img>
           </Link>
-          <Link to="/logout">
-            <img src="logout.svg" width="50" height="50"></img>
-          </Link>
-        </>
-      )}
-    </nav>
+        )}
+        {user && (
+          <>
+            <Link to="/profile">
+              <img src="person.svg" width="50" height="50"></img>
+            </Link>
+            <Link to="/logout">
+              <img src="logout.svg" width="50" height="50"></img>
+            </Link>
+          </>
+        )}
+      </nav>
+    </>
   );
 };
 

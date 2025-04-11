3edb33f27d0288591046809d00ff949c6cacf500
Arthur Chardon <arthur.chardon74@gmail.com>
2025-04-11 12:15:26 +0200

    fix: remoev unused import

--- frontend/src/components/WordleGame/WordleGame.tsx
+++ frontend/src/components/WordleGame/WordleGame.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { LetterStatus } from "../../types/words";
 import Word from "./Word/Word";
 import "./WordleGame.css";
